275c8a19b2ebbcf0828a72132c954a0a19bf2c35
Brian Knight <bknight84@gmail.com>
2022-09-26 16:41:45 -0600

    working again

--- backend/javaforce5-beta/src/main/java/App/TestApp.java
+++ backend/javaforce5-beta/src/main/java/App/TestApp.java
@@ -8,6 +8,7 @@ import main.java.dao.CrimeDao;
 import main.java.dao.CriminalRecordDao;
 import main.java.models.CriminalRecord;
 import main.java.models.requests.GetCriminalRecordRequest;
+import main.java.models.requests.GetCriminalsRecordsByStateRequest;
 
 import java.util.List;
 
@@ -18,14 +19,26 @@ public class TestApp {
         CriminalRecordDao criminalRecordDao = new CriminalRecordDao(mapper);
         CrimeDao crimeDao = new CrimeDao(mapper);
 
+        //Test get one record
         GetCriminalRecordRequest getBartFart = GetCriminalRecordRequest.builder()
                 .withSsn("777-66-5555")
                 .build();
-
         GetCriminalRecordActivity getCriminalRecordActivity = new GetCriminalRecordActivity(criminalRecordDao);
+        CriminalRecord bart = getCriminalRecordActivity.handleRequest(getBartFart, null);
+        System.out.println(bart);
+        System.out.println("-".repeat(80));
 
-        CriminalRecord bartFart = getCriminalRecordActivity.handleRequest(getBartFart, null);
-        System.out.println(bartFart);
+        //Test get records by state:
+        GetCriminalsRecordsByStateRequest getRecordsCalifornia = GetCriminalsRecordsByStateRequest.builder()
+                .withState("CA")
+                .build();
+        GetCriminalsRecordsByStateActivity getCriminalsRecordsByStateActivity = new GetCriminalsRecordsByStateActivity(criminalRecordDao);
+        List<CriminalRecord> result = getCriminalsRecordsByStateActivity.handleRequest(getRecordsCalifornia, null);
+        for(CriminalRecord criminalRecord : result) {
+            System.out.println(criminalRecord);
+        }
+        System.out.println("-".repeat(80));
+
     }
 
 }
